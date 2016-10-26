84bf0ba67c5a243a84133fb9a609db32342b1a91
Brian Black <bpblack04@mac.com>
2016-10-25 20:05:30 -0700

    Actually getting the roles string calculation right.

--- app/models/roster_spot.rb
+++ app/models/roster_spot.rb
@@ -6,25 +6,26 @@ class RosterSpot < ApplicationRecord
   validates_uniqueness_of :season_id, scope: :user_id
 
   def team_roles_string
-    roles = []
-    has_roles_method = user.roles.loaded? ? :has_cached_role? : :has_role?
-    Rails.application.config.team_roles.each do |role|
-      roles << role[:name] if user.method(has_roles_method).call(role[:role], role[:resourced] ? self : nil)
-    end
-    roles.join(', ')
+    return roles_str(Rails.application.config.team_roles)
   end
 
   def team_extra_roles_string
-    roles = []
-    Rails.application.config.team_extra_roles do |role|
-      roles << role[:name] if user.method(has_roles_method).call(role[:role], role[:resourced] ? self : nil)
-    end
-    roles.join(', ')
+    return roles_str(Rails.application.config.team_extra_roles)
   end
 
   def team_all_roles_string
     r = team_roles_string
     e = team_extra_roles_string
     e.blank? ? r : "#{r}, #{e}"
+  end
+
+  private 
+  def roles_str roles_array
+    roles = []
+    has_roles_method = user.roles.loaded? ? :has_cached_role? : :has_role?
+    roles_array.each do |role|
+      roles << role[:name] if user.method(has_roles_method).call(role[:role], role[:resourced] ? self : nil)
+    end
+    roles.join(', ')
   end
 end

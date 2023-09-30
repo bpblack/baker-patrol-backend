354f8d7205f31bf6656a81c729b0fb620dc39093
Brian Black <bblack@veracode.com>
2023-09-29 22:28:51 -0700

    Getting rid of the old migrations.

--- db/migrate/20160903032808_add_role_ref_to_patrol_responsibility.rb
+++
@@ -1,5 +0,0 @@
-class AddRoleRefToPatrolResponsibility < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :patrol_responsibilities, :role, foreign_key: true
-  end
-end
--- db/migrate/20201116031544_add_reserve_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddReserveToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :reserve, :boolean, default: false
-  end
-end

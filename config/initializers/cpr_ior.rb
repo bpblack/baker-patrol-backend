e238a0e667354b9add869719864dee568b65a12e
Brian Black <bblack@veracode.com>
2023-10-03 21:50:08 -0700

    Comment out so I can use rails c to initialize the cpr_ior user.

--- config/initializers/cpr_ior.rb
+++ config/initializers/cpr_ior.rb
@@ -1,7 +1,7 @@
 Rails.configuration.after_initialize do
-  ior = User.joins(:roles).where(roles: {name: :cprior})[0]
-  Rails.application.config.cpr_ior = {
-    name: ior.name,
-    email: ior.email
-  }
+  # ior = User.joins(:roles).where(roles: {name: :cprior})[0]
+  # Rails.application.config.cpr_ior = {
+  #   name: ior.name,
+  #   email: ior.email
+  # }
 end

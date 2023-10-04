70eb6ffffeb1a29e4b716c11882d68a65862c2ad
Brian Black <bblack@veracode.com>
2023-10-03 21:47:33 -0700

    Setting a dummy value.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,4 +82,6 @@ Rails.application.configure do
     event_location: 'Mt. Baker Ski Area, Mount Baker Highway, Deming, WA',
     secrets: File.open(File.join(Rails.root, 'config', 'google_client_secret.json'), 'r') { |file| JSON.load(file.read) }
   }
+
+  config.cpr_ior = nil
 end

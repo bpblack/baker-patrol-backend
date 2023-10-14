c97efbcb4ae214274be2e20ff093e3fada0e7d51
Brian Black <bblack@veracode.com>
2023-10-13 23:58:06 -0700

    Season seeding controller action.

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -1,6 +1,15 @@
 class SeasonsController < ApplicationController
+  require 'JSON'
+  require 'CSV'
   include BakerDataService
   before_action :authenticate_user
+  rescue_from ActiveRecord::ActiveRecordError, with: :season_invalid
+  rescue_from IOError, with: :season_invalid
+  rescue_from SystemCallError, with: :season_invalid
+  rescue_from JSON::JSONError, with: :season_invalid
+  rescue_from CSV::MalformedCSVError, with: :season_invalid
+  rescue_from CSV::Parser::InvalidEncoding, with: :season_invalid
+  rescue_from CSV::Parser::UnexpectedError, with: :season_invalid
 
   def latest 
     authorize Season
@@ -10,5 +19,18 @@ class SeasonsController < ApplicationController
 
   def create
     authorize Season
+    sd = DateTime.parse(params[:start]).to_date
+    ed = DateTime.parse(params[:end]).to_date
+    season = BakerDataService::SeasonData.new(sd, ed, params[:roster], params[:team].to_sym)
+    season.seed()
+    @latest = Season.last
+    render :latest, formats: [:json], status: :ok
   end
+
+  private
+
+  def season_invalid(exception)
+    render json: exception.message, status: :bad_request 
+  end
+
 end
--- lib/modules/baker_data_service.rb
+++ lib/modules/baker_data_service.rb
@@ -11,7 +11,7 @@ module BakerDataService
 
   class SeasonData
     require 'csv'
-    def initialize(first, last, roster)
+    def initialize(first, last, roster_upload, start_team)
       @first = first
       @last = last
       @name = "Winter #{first.year}/#{last.year}"
@@ -27,54 +27,64 @@ module BakerDataService
         avy2: false,
         mtr: false
       }
-      base_role_ids = {
-        admin: Role.find_by(name: :admin).id,
-        onhill: Role.find_by(name: :onhill).id,
-        host: Role.find_by(name: :host).id
-      }
-      @responsibility_ids = {
-        lead: self.class.find_or_create_responsibility(name: 'Team leader', version: 15, role_id: base_role_ids[:onhill]),
-        p1: self.class.find_or_create_responsibility(name: 'P1', version: 15, role_id: base_role_ids[:onhill]),
-        p2: self.class.find_or_create_responsibility(name: 'P2', version: 15, role_id: base_role_ids[:onhill]),
-        p3: self.class.find_or_create_responsibility(name: 'P3', version: 15, role_id: base_role_ids[:onhill]),
-        p4: self.class.find_or_create_responsibility(name: 'P4', version: 15, role_id: base_role_ids[:onhill]),
-        p5: self.class.find_or_create_responsibility(name: 'P5', version: 15, role_id: base_role_ids[:onhill]),
-        s1: self.class.find_or_create_responsibility(name: 'S1', version: 15, role_id: base_role_ids[:onhill]),
-        s2: self.class.find_or_create_responsibility(name: 'S2', version: 15, role_id: base_role_ids[:onhill]),
-        s3: self.class.find_or_create_responsibility(name: 'S3', version: 15, role_id: base_role_ids[:onhill]),
-        s4: self.class.find_or_create_responsibility(name: 'S4', version: 15, role_id: base_role_ids[:onhill]),
-        s5: self.class.find_or_create_responsibility(name: 'S5', version: 15, role_id: base_role_ids[:onhill]),
-        mw1: self.class.find_or_create_responsibility(name: 'Midweek 1', version: 15, role_id: base_role_ids[:onhill]),
-        mw2: self.class.find_or_create_responsibility(name: 'Midweek 2', version: 15, role_id: base_role_ids[:onhill]),
-        mw3: self.class.find_or_create_responsibility(name: 'Midweek 3', version: 15, role_id: base_role_ids[:onhill]),
-        mw4: self.class.find_or_create_responsibility(name: 'Midweek 4', version: 15, role_id: base_role_ids[:onhill]),
-        h1: self.class.find_or_create_responsibility(name: 'Host 1', version: 1, role_id: base_role_ids[:host]),
-        h2: self.class.find_or_create_responsibility(name: 'Host 2', version: 1, role_id: base_role_ids[:host]),
-        h3: self.class.find_or_create_responsibility(name: 'Host 3', version: 1, role_id: base_role_ids[:host]),
-        h4: self.class.find_or_create_responsibility(name: 'Host 4', version: 1, role_id: base_role_ids[:host]),
-        #base: find_or_create_responsibility(name: 'Base', version: 15, role_id: base_role_ids[:onhill])
-      }
-      @teams = {
-        trainer: Team.find_by(name: 'Trainer Team').id,
-        midweek: Team.find_by(name: 'Midweek Team').id,
-        a: Team.find_by(name: 'A Team').id,
-        b: Team.find_by(name: 'B Team').id,
-        c: Team.find_by(name: 'C Team').id,
-        d: Team.find_by(name: 'D Team').id,
-        host: Team.find_by(name: 'Host Team').id,
-      }
-      #load the midweek patrols from csv
-      @midweek_patrols = self.class.load_patrol_csv('config/subs/midweek_patrols.csv')
-      @midweek_patrols += @midweek_patrols #duplicate for the second team in the roster
-      @midweek_patrols.unshift(Array.new(@midweek_patrols[0].length * 2, :lead)) #double the leader duties 
-      #load the weekend patrols from csv
-      @weekend_patrols = self.class.load_patrol_csv('config/subs/weekend_patrols.csv')
-      @weekend_patrols.unshift(Array.new(@weekend_patrols[0].length, :lead))
-      #load the host patrols from csv
-      @host_patrols = self.class.load_patrol_csv('config/subs/host_patrols.csv')
-      #load the roster form json
-      @roster = ActiveSupport::JSON.decode(roster)
-      puts @roster
+
+      begin
+        base_role_ids = {
+          admin: Role.find_by(name: :admin).id,
+          onhill: Role.find_by(name: :onhill).id,
+          host: Role.find_by(name: :host).id
+        }
+        @responsibility_ids = {
+          lead: self.class.find_or_create_responsibility(name: 'Team leader', version: 15, role_id: base_role_ids[:onhill]),
+          p1: self.class.find_or_create_responsibility(name: 'P1', version: 15, role_id: base_role_ids[:onhill]),
+          p2: self.class.find_or_create_responsibility(name: 'P2', version: 15, role_id: base_role_ids[:onhill]),
+          p3: self.class.find_or_create_responsibility(name: 'P3', version: 15, role_id: base_role_ids[:onhill]),
+          p4: self.class.find_or_create_responsibility(name: 'P4', version: 15, role_id: base_role_ids[:onhill]),
+          p5: self.class.find_or_create_responsibility(name: 'P5', version: 15, role_id: base_role_ids[:onhill]),
+          s1: self.class.find_or_create_responsibility(name: 'S1', version: 15, role_id: base_role_ids[:onhill]),
+          s2: self.class.find_or_create_responsibility(name: 'S2', version: 15, role_id: base_role_ids[:onhill]),
+          s3: self.class.find_or_create_responsibility(name: 'S3', version: 15, role_id: base_role_ids[:onhill]),
+          s4: self.class.find_or_create_responsibility(name: 'S4', version: 15, role_id: base_role_ids[:onhill]),
+          s5: self.class.find_or_create_responsibility(name: 'S5', version: 15, role_id: base_role_ids[:onhill]),
+          mw1: self.class.find_or_create_responsibility(name: 'Midweek 1', version: 15, role_id: base_role_ids[:onhill]),
+          mw2: self.class.find_or_create_responsibility(name: 'Midweek 2', version: 15, role_id: base_role_ids[:onhill]),
+          mw3: self.class.find_or_create_responsibility(name: 'Midweek 3', version: 15, role_id: base_role_ids[:onhill]),
+          mw4: self.class.find_or_create_responsibility(name: 'Midweek 4', version: 15, role_id: base_role_ids[:onhill]),
+          h1: self.class.find_or_create_responsibility(name: 'Host 1', version: 1, role_id: base_role_ids[:host]),
+          h2: self.class.find_or_create_responsibility(name: 'Host 2', version: 1, role_id: base_role_ids[:host]),
+          h3: self.class.find_or_create_responsibility(name: 'Host 3', version: 1, role_id: base_role_ids[:host]),
+          h4: self.class.find_or_create_responsibility(name: 'Host 4', version: 1, role_id: base_role_ids[:host]),
+          #base: find_or_create_responsibility(name: 'Base', version: 15, role_id: base_role_ids[:onhill])
+        }
+        @teams = {
+          trainer: Team.find_by(name: 'Trainer Team').id,
+          midweek: Team.find_by(name: 'Midweek Team').id,
+          a: Team.find_by(name: 'A Team').id,
+          b: Team.find_by(name: 'B Team').id,
+          c: Team.find_by(name: 'C Team').id,
+          d: Team.find_by(name: 'D Team').id,
+          host: Team.find_by(name: 'Host Team').id,
+        }
+        #load the midweek patrols from csv
+        @midweek_patrols = self.class.load_patrol_csv('config/subs/midweek_patrols.csv')
+        @midweek_patrols += @midweek_patrols #duplicate for the second team in the roster
+        @midweek_patrols.unshift(Array.new(@midweek_patrols[0].length * 2, :lead)) #double the leader duties 
+        #load the weekend patrols from csv
+        @weekend_patrols = self.class.load_patrol_csv('config/subs/weekend_patrols.csv')
+        @weekend_patrols.unshift(Array.new(@weekend_patrols[0].length, :lead))
+        #load the host patrols from csv
+        @host_patrols = self.class.load_patrol_csv('config/subs/host_patrols.csv')
+        #load the roster form json
+        roster_data = roster_upload.read
+        @roster = ActiveSupport::JSON.decode(roster_data)
+        Rails.logger.debug @roster['d']
+        a_weekend = [@teams[:midweek], @teams[:a], @teams[:b]]
+        c_weekend = [@teams[:midweek], @teams[:c], @teams[:d]]
+        @patrol_teams = start_team == :a ? a_weekend+c_weekend : c_weekend+a_weekend
+      rescue Exception => e
+        Rails.logger.error "DATA SERVICE INITIALIZATION: #{e.message}"
+        raise
+      end
     end
 
     def email_new_user(user)
@@ -182,58 +192,61 @@ module BakerDataService
     end
     
     def mapRoster(team_roster, patrol_table)
-      team_roster.each_with_index.map do |m, i| 
-        [m.nil? ? nil : {first_name: m[0], last_name: m[1], email: m[2], phone: m[3], password: DataService::random_pass}, patrol_table[i], m[4].nil? ? [] : m[4].map{ |e| e.to_sym}]
+      team_roster.each_with_index.map do |m, i|
+        if (m.nil?)
+          [nil, patrol_table[i]]
+        else  
+          [{first_name: m[0], last_name: m[1], email: m[2], phone: m[3], password: BakerDataService::random_pass}, patrol_table[i], (m[4].nil? ? [] : m[4].map{ |e| e.to_sym})]
+        end
       end
     end 
 
     def seed()
       ActiveRecord::Base.transaction do
         # create the season
-        puts "Creating #{@name} season..."
+        Rails.logger.info "Creating #{@name} season..."
         @season_id = Season.create!(name: @name, start: @first, end: @last).id
         # create the duty days
-        puts "Creating duty days..."
-        patrol_teams = [@teams[:midweek], @teams[:a], @teams[:b], @teams[:midweek], @teams[:c], @teams[:d]]
-        (@first..@last).filter_map { |x| x if (x.friday? || x.saturday? || x.sunday?) }.zip(patrol_teams.cycle).each do |dd|
+        Rails.logger.info "Creating duty days..."
+        (@first..@last).filter_map { |x| x if (x.friday? || x.saturday? || x.sunday?) }.zip(@patrol_teams.cycle).each do |dd|
           DutyDay.create!(season_id: @season_id, date: dd[0], team_id: dd[1])
         end
         #seed trainers
-        puts "Seeding Trainer team..."
+        Rails.logger.info "Seeding Trainer team..."
         seed_members(:onhill, @teams[:trainer], mapRoster(@roster['trainer'], []), [])
         #seed midweek
-        puts "Seeding Midweek team..."
+        Rails.logger.info "Seeding Midweek team..."
         midweek = @teams[:midweek]
         midweek_duty_day_ids = DutyDay.where(season_id: @season_id, team_id: midweek).order(date: :asc).pluck(:id)
         seed_midweek(:onhill, midweek, mapRoster(@roster['midweek'], @midweek_patrols), midweek_duty_day_ids)
         #seed a
-        puts "Seeding A team..."
+        Rails.logger.info "Seeding A team..."
         a = @teams[:a]
         a_duty_day_ids = DutyDay.where(season_id: @season_id, team_id: a).order(date: :asc).pluck(:id) #add rotation for this season
         seed_weekend(:onhill, a, mapRoster(@roster['a'], @weekend_patrols), a_duty_day_ids)
         seed_members(:host, a, mapRoster(@roster['a_hosts'], @host_patrols), a_duty_day_ids)
         #seed b
-        puts "Seeding B team..."
+        Rails.logger.info "Seeding B team..."
         b = @teams[:b]
         b_duty_day_ids = DutyDay.where(season_id: @season_id, team_id: b).order(date: :asc).pluck(:id)
         seed_weekend(:onhill, b, mapRoster(@roster['b'], @weekend_patrols), b_duty_day_ids)
         seed_members(:host, b, mapRoster(@roster['b_hosts'], @host_patrols), b_duty_day_ids)
         #seed c
-        puts "Seeding C team..."
+        Rails.logger.info "Seeding C team..."
         c = @teams[:c]
         c_duty_day_ids = DutyDay.where(season_id: @season_id, team_id: c).order(date: :asc).pluck(:id)
         seed_weekend(:onhill, c, mapRoster(@roster['c'], @weekend_patrols), c_duty_day_ids)
         seed_members(:host, c, mapRoster(@roster['c_hosts'], @host_patrols), c_duty_day_ids)
         #seed d
-        puts "Seeding D team..."
+        Rails.logger.info "Seeding D team..."
         d = @teams[:d]
         d_duty_day_ids = DutyDay.where(season_id: @season_id, team_id: d).order(date: :asc).pluck(:id)
         seed_weekend(:onhill, d, mapRoster(@roster['d'], @weekend_patrols), d_duty_day_ids)
         seed_members(:host, d, mapRoster(@roster['d_hosts'], @host_patrols), d_duty_day_ids)
-        puts "#{@name} seeding completed!"
-        #only for testing
+        Rails.logger.info "#{@name} seeding completed!"
       rescue ActiveRecord::RecordInvalid => e
-        puts "FAILED to seed: #{e.message}"
+        Rails.logger.error "FAILED to seed: #{e.message}"
+        raise
       end
     end
 
